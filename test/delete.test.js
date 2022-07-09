31320f52158eadccd3bae8c97865ed46ab72aacb
Dmitrii Manushin <direct2dmnsh@gmail.com>
2022-07-09 11:22:33 +0200

    make some bugfixes and add tests

--- test/delete.test.js
+++ test/delete.test.js
@@ -1,9 +1,17 @@
 const {deleteRow, deleteColumn} = require("../lib/delete")
 
-test("Delete a row from csv", () => {
+test("Delete a row from csv with options", () => {
     expect(deleteRow("a,b,c\n1,2,3\n4,5,6", 2, {headers: true})).toBe("a,b,c\n1,2,3")
 })
 
-test("Delete a column from csv", () => {
+test("Delete a row from csv without options", () => {
+    expect(deleteRow("a,b,c\n1,2,3\n4,5,6", 2)).toBe("a,b,c\n1,2,3")
+})
+
+test("Delete a column from csv with options", () => {
+    expect(deleteColumn("a,b,c\n1,2,3\n4,5,6", 2, {headers: true})).toBe("a,c\n1,3\n4,6")
+})
+
+test("Delete a column from csv without options", () => {
     expect(deleteColumn("a,b,c\n1,2,3\n4,5,6", 2)).toBe("a,c\n1,3\n4,6")
 })
--- test/insert.test.js
+++ test/insert.test.js
@@ -1,9 +1,17 @@
 const { insertColumn, insertRow } = require("../lib/insert")
 
-test("Insert string column to csv", () => {
+test("Insert string column to csv with options", () => {
     expect(insertColumn("a,b,c\n1,2,3", "d,4", {headers: true})).toBe("a,b,c,d\n1,2,3,4")
 })
 
-test("Insert string row to csv", () => {
+test("Insert string column to csv without options", () => {
+    expect(insertColumn("a,b,c\n1,2,3", "d,4")).toBe("a,b,c,d\n1,2,3,4")
+})
+
+test("Insert string row to csv with options", () => {
     expect(insertRow("a,b,c\n1,2,3", "4,5,6", {headers: true})).toBe("a,b,c\n1,2,3\n4,5,6")
+})
+
+test("Insert string row to csv without options", () => {
+    expect(insertRow("a,b,c\n1,2,3", "4,5,6")).toBe("a,b,c\n1,2,3\n4,5,6")
 })
